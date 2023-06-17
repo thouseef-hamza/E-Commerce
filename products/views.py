05099f017810f02cbf1e308557ffae027731635b
Thouseef <thousithouseef731@gmail.com>
2023-06-17 12:55:37 +0530

    fix bugs

--- products/views.py
+++ products/views.py
@@ -38,7 +38,7 @@ def products(request,category_slug=None):
 
 @never_cache
 def product_detail(request,category_slug,product_slug):
-    user_profile = get_object_or_404(UserProfile,user=request.user)
+    user_profile = UserProfile.objects.get(user=request.user)
     try:
         single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
